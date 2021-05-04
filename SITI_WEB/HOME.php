a543ae558bf9e27b96057e177ff5bfc15db62cb7
ALFO2 <pauloalarcon76@gmail.com>
2021-05-04 08:02:33 +0200

    4/5

---
+++ SITI_WEB/HOME.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sicilypost</title>
+     <link rel="stylesheet" href="SITI_WEB/CSS/GENERALE.css">
+  </head>
+  <body>
+
+  <header>
+    <div id="sidebar">
+      <div >
+          <button class="button" type="button" name="button"><h2>HOME</h2></button>
+      </div>
+      <div >
+           <button class="button" id="button2" type="button" name="button"><h2>Accedi</h2> </button>
+      </div>
+
+    </div>
+    <div id="title">
+       <div id="Sicilypost" class="">
+          <h1 ><spam>Sicily</spam>post</h1>
+       </div>
+
+
+    </div>
+  </header>
+  <div id="content">
+    <article id="contenuto1" class="">
+      <h2>ciao</h2>
+    </article>
+    <article id="contenuto2" class="">
+     <h2>ciao</h2>
+    </article>
+  </div>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+ //session_start();
+//require 'Bariables/VGeneral.php';
+//require 'Function/sql.php';
+$f = isset($_REQUEST['f'])? (string) $_REQUEST['f'] : "Home";
+switch( $f ){
+	case 'Home': HOME();break;
+	case 'Sign_in': SIGN_IN(); break;
+	default: Home();
+}
+
+
+
+function HOME()
+{
+   require 'SITI_WEB/HOME.php';
+}
+function SIGN_IN()
+{
+   require 'SITI_WEB/SIGN_IN.php';
+}
+
+  ?>
